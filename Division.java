3b349c86476ac29be76e7daa2b1abae21d74b2db
Kongari Sivaram <sivaramkongari@gmail.com>
2026-03-16 19:12:20 +0530

    String added

--- Division.java
+++ Division.java
@@ -5,12 +5,12 @@ class Div{
         double d1 = 10;
         double d2 = 2;
         double res1 = d1/d2;
-        System.out.println(res1);
+        System.out.println("Double division is : "+res1);
 
         int a = 20;
         int b = 5;
         int res2 = a%b;
-        System.out.println(res2);
+        System.out.println("Integer Division is : "+res2);
         
     }
 }

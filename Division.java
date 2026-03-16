88dedb3f6050107acfe6c8a50251fdaa87a311ea
Kongari Sivaram <sivaramkongari@gmail.com>
2026-03-16 19:09:23 +0530

    Divided two integers

--- Division.java
+++ Division.java
@@ -4,8 +4,13 @@ class Div{
 
         double d1 = 10;
         double d2 = 2;
-        double res = d1/d2;
-        System.out.println(res);
+        double res1 = d1/d2;
+        System.out.println(res1);
+
+        int a = 20;
+        int b = 5;
+        int res2 = a%b;
+        System.out.println(res2);
         
     }
 }

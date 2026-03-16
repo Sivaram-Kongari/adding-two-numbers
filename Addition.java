ab510e244e02f3becb40a4d04cc697af67b8b9d0
Kongari Sivaram <sivaramkongari@gmail.com>
2026-03-16 19:22:24 +0530

    Adding two doubles

--- Addition.java
+++ Addition.java
@@ -11,6 +11,12 @@ class Add{
         int d = 4;
         int res1 = c*d;
         System.out.println(res1);
+
+        double d1 = 5;
+        double d2 = 10;
+        double res2 = d1+d2;
+        System.out.println(res2);
+
         
     }
 }

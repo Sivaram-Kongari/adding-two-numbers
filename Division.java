1e8e618c6851bc8e3dadaa352d178725bfd4ded4
Kongari Sivaram <sivaramkongari@gmail.com>
2026-03-16 19:05:50 +0530

    Division is done

---
+++ Division.java
@@ -0,0 +1,11 @@
+class Div{
+
+    public static void main(String[] args) {
+
+        double d1 = 10;
+        double d2 = 2;
+        double res = d1/d2;
+        System.out.println(res);
+        
+    }
+}

7695bf9377383662884ecd3f35ea412691e47671
Kongari Sivaram <sivaramkongari@gmail.com>
2026-03-17 10:09:23 +0530

    Multiplied double values

--- Multiple.java
+++ Multiple.java
@@ -6,5 +6,10 @@ class Mul{
         int b = 3;
         int rs = a*b;
         System.out.println(rs);
+
+        double d1 = 4;
+        double d2 = 5;
+        double res = d1*d2;
+        System.out.println(res);
     }
 }
